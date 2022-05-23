0b98dfdb81cf9ae6f2efd0d64618500167990c79
PersesTitan <torampersestitan@gmail.com>
2022-05-24 00:32:27 +0900

    commit

--- app/src/main/java/com/ocr/ocrproject/MainActivity.java
+++ app/src/main/java/com/ocr/ocrproject/MainActivity.java
@@ -1,10 +1,18 @@
 package com.ocr.ocrproject;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.ActionMode;
+import android.view.View;
 import android.widget.Adapter;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 
 import com.ocr.ocrproject.databinding.ActivityMainBinding;
@@ -13,6 +21,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String shared = "filed";
+    private final String save = "SAVE";
 
     String dataPath = "";
     String imagePath = "";
@@ -30,19 +41,42 @@ public class MainActivity extends AppCompatActivity {
 
         setSpinner();
         binding.textView.setText(null);
-        binding.camara.setOnClickListener(v -> {
-
-        });
+        binding.camara.setOnClickListener(this::startCamera);
 
     }
 
+    private void startCamera(View view) {
+        startActivityForResult(new Intent(MediaStore.ACTION_IMAGE_CAPTURE), 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        System.out.println("MainActivity.onActivityResult");
+        if (requestCode == 0 && resultCode == RESULT_OK && data != null) {
+            Bundle bundle = data.getExtras();
+            Bitmap bitmap = (Bitmap) bundle.get("data");
+            binding.imageView.setImageBitmap(bitmap);
+        }
+    }
+
     private void setSpinner() {
-        List<String> list = new ArrayList<>();
-        list.add("한국어");
-        list.add("영어");
-        list.add("일본어");
+        SharedPreferences sharedPreferences = getSharedPreferences(shared, 0);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        String[] list = {"한국어", "영어", "일본어"};
         int layout = android.R.layout.simple_list_item_1;
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, layout, list);
         binding.languageSpinner.setAdapter(adapter);
+        binding.languageSpinner.setSelection(sharedPreferences.getInt(save, 0));
+        binding.languageSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                editor.putInt(save, i);
+                editor.apply();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) { }
+        });
     }
 }
